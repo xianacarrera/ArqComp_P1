dccfbadccd2179e104e05ef4fe2212870692ca1b
Xiana Carrera Alonso <xcarrera.alonso@gmail.com>
2022-03-16 12:44:35 +0100

    Resultados (de prueba)
    No sirven, solo son para probar gráficas

--- bash_script.sh
+++ bash_script.sh
@@ -2,19 +2,16 @@
 
 factor_L=(0.5 1.5 0.5 0.75 2.0 4.0 8.0)
 cache=(1 1 2 2 2 2 2)
-lista_D="1 5 10"
+lista_D="1 5 10 50 95"
 
 for i in {0..6}
 do
     for D in $lista_D
     do
-        for j in {0..6}
+        for k in {0..9}
         do
-            for k in {0..9}
-            do
-                ./accesos_cache ${factor_L[i]} ${cache[i]} 1
-            done
-            ./mediana
+            ./accesos_cache ${factor_L[i]} ${cache[i]} $D
         done
+        ./mediana
     done        
 done
