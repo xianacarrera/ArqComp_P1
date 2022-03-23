6e73bacc5d121f9749102bc8d2ddd93e9c059f35
anacarsi <73550326+anacarsi@users.noreply.github.com>
2022-03-23 01:02:07 +0100

    comentarios en mediana2.c
    revisar la necesidad del fscanf sobre los vectores L[], D[]... 10 veces, linea 41

---
+++ mediana2.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NL 7       // Número de valores de L a probar
+#define NE 10      // Número de experimentos para cada par (D, L)
+#define ND 5       // Número de valores que toma D
+
+
+void sort(double CK[NE]);
+
+
+
+int main(int argc, char * argv[]){
+    FILE *res_temporales;
+    FILE *res_finales;
+    int i, j;
+    double mediana;
+    long L[NL*ND];
+    int D[NL*ND];
+    long R[NL*ND];
+    long N[NL*ND];
+    /* Matriz de tiempos por acceso
+     * Cada columna se corresponde a un experimento para el que se han realizado NL*ND = 35 medidas
+     */
+    double CK[NL*ND][NE]; 
+
+    if ((res_temporales = fopen("res_temporales.txt", "r")) == NULL){
+        fprintf(stderr, "Error al abrir el archivo de resultados temporales\n");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((res_finales = fopen("res_totales.txt", "a")) == NULL){
+        fprintf(stderr, "Error al abrir el archivo de resultados finales\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Lectura de los 350 experimentos realizados, 10 experimentos para cada par (D,L)
+    for (j = 0; j < NE; j++){
+    	// Para cada experimento j, se almacenan los 35 tiempos correspondientes a los 35 pares (D,L) distintos por acceso calculados en CK[][j] 
+        for (i = 0; i < NL * ND; i++){
+            fscanf(res_temporales, "%ld %d %ld %ld %lf", &L[i], &D[i], &R[i], &N[i], &CK[i][j]);
+        }
+    }
+
+    for (i = 0; i < NL * ND; i++){
+	// Ordenación de los 10 tiempos de acceso obtenidos para cada par (D,L) para la obtención de la mediana
+        sort(CK[i]);
+        mediana = (CK[i][4] + CK[i][5]) / ((double) 2);
+        fprintf(res_finales, "%ld %d %ld %ld %f\n", L[i], D[i], R[i], N[i], mediana);
+    }
+
+    freopen("res_temporales.txt", "w", res_temporales);
+
+    fclose(res_temporales);
+    fclose(res_finales);
+
+    exit(EXIT_SUCCESS);
+}
+
+/*
+ * Función que ordena en orden ascendente los elementos de un vector para el cálculo de la mediana
+ * @param CK Array de los 10 resultados obtenidos en cada experimento para cada par (D,L)
+ */
+void sort(double CK[NE]){
+    int i, j;
+    double temp;
+
+    for (i = 0; i < NE; i++){
+        for (j = 0; j < NE -1; j++){
+            if (CK[j] > CK[j+1]){
+                temp = CK[j];
+                CK[j] = CK[j+1];
+                CK[j+1] = temp;
+            }
+        }
+    }
+}
