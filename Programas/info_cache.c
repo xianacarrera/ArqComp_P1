9506cb61c566bb964ff51c5b40e36d1d1f541994
Xiana <xcarrera.alonso@gmail.com>
2022-08-17 22:03:40 +0200

    Actualizar estructura del repo

---
+++ Programas/info_cache.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>               // Función log
+#include <unistd.h>
+
+/*
+ * Xiana Carrera Alonso, Ana Carsi González
+ * Arquitectura de Computadores - Curso 2021/2022
+ * Práctica 1 - Jerarquía de memoria y comportamiento de memoria cache: Estudio del efecto de la localidad de los
+ * accesos a memoria en las prestaciones de programas en microprocesadores
+ *
+ * Este programa permite comprobar las características de los niveles L1 (datos e instrucciones), L2 y L3 de la
+ * jerarquía cache del equipo. En concreto, se determinan el tamaño total, el tamaño de línea y el número de vías
+ * de cada nivel.
+ * Aunque realmente en el estudio no son necesarios todos estos valores, se ha querido comprobarlos igualmente para
+ * asegurar la correcta comprensión de la estructura particular de memoria en el equipo de ejecución.
+ */
+
+
+int main(int argc, char * argv[]) {
+    long total;                 // Tamaño total de cada nivel, en bytes
+    long vias;                  // Número de vías de cada nivel
+    long tam_linea;             // Tamaño de línea de cada nivel
+    long n_lineas_L1I;          // Número de líneas de la caché L1 de instrucciones
+    long n_lineas_L1D;          // Número de líneas de la caché L1 de datos
+    long n_lineas_L2;           // Número de líneas de la caché L2
+    long n_conjuntos_L1I;       // Número de conjuntos de la caché L1 de instrucciones
+    long n_conjuntos_L1D;       // Número de conjuntos de la caché L1 de datos
+    long n_conjuntos_L2;        // Número de conjuntos de la caché L2
+
+    /***** Caché L1 de instrucciones *****/
+
+    // Tamaño total
+    total = sysconf(_SC_LEVEL1_ICACHE_SIZE);
+    // Nº de vías
+    vias = sysconf(_SC_LEVEL1_ICACHE_ASSOC);
+    // Tamaño de línea
+    tam_linea = sysconf(_SC_LEVEL1_ICACHE_LINESIZE);
+
+    printf("\n**** Datos cache L1 de instrucciones: ****\n");
+    printf("\tTamanho total: %ld = 2^(%d)\n", total, (int) (log(total) / log(2)));
+    printf("\tNº de vías: %ld\n", vias);
+    printf("\tTamanho de linea: %ld\n\n", tam_linea);
+
+    // El número de líneas será igual al tamaño total dividido por el tamaño de cada línea cache
+    n_lineas_L1I = total / tam_linea;
+
+    // El número de conjuntos será igual al número de líneas dividido por el número de vías
+    n_conjuntos_L1I = n_lineas_L1I / vias;
+
+    /***** Caché L1 de datos *****/
+
+    total = sysconf(_SC_LEVEL1_DCACHE_SIZE);
+    vias = sysconf(_SC_LEVEL1_DCACHE_ASSOC);
+    tam_linea = sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+
+    printf("**** Datos cache L1 de datos: ****\n");
+    printf("\tTamanho total: %ld = 2^(%d)\n", total, (int) (log(total) / log(2)));
+    printf("\tNº de vías: %ld\n", vias);
+    printf("\tTamanho de linea: %ld\n\n", tam_linea);
+
+    n_lineas_L1D = total / tam_linea;
+    n_conjuntos_L1D = n_lineas_L1D / vias;
+
+    /***** Caché L2 *****/
+
+    total = sysconf(_SC_LEVEL2_CACHE_SIZE);
+    vias = sysconf(_SC_LEVEL2_CACHE_ASSOC);
+    tam_linea = sysconf(_SC_LEVEL2_CACHE_LINESIZE);
+
+    printf("**** Datos cache L2: ****\n");
+    printf("\tTamanho total: %ld = 2^(%d)\n", total, (int) (log(total) / log(2)));
+    printf("\tNº de vías: %ld\n", vias);
+    printf("\tTamanho de linea: %ld\n\n", tam_linea);
+
+    n_lineas_L2 = total / tam_linea;
+    n_conjuntos_L2 = n_lineas_L2 / vias;
+
+
+    /***** Caché L3 *****/
+
+    total = sysconf(_SC_LEVEL3_CACHE_SIZE);
+    vias = sysconf(_SC_LEVEL3_CACHE_ASSOC);
+    tam_linea = sysconf(_SC_LEVEL3_CACHE_LINESIZE);
+
+    printf("**** Datos cache L3: ****\n");
+    printf("\tTamanho total: %ld = 2^(%d)\n", total, (int) (log(total) / log(2)));
+    printf("\tNº de vías: %ld\n", vias);
+    printf("\tTamanho de linea: %ld\n\n", tam_linea);
+
+    // Se imprime el número de líneas de cada cache
+    printf("S1I = nº líneas L1I = %ld\n", n_lineas_L1I);
+    printf("S1D = nº líneas L1D = %ld\n", n_lineas_L1D);
+    printf("S2 = nº líneas L2 = %ld\n", n_lineas_L2);
+    printf("S3 = nº líneas L3 = %ld\n\n", total / tam_linea);       // total y tam_linea hacen referencia a la L3
+
+    // E imprimimos también el número de conjuntos
+    printf("Nº conjuntos L1I = %ld\n", n_conjuntos_L1I);
+    printf("Nº conjuntos L1D = %ld\n", n_conjuntos_L1D);
+    printf("Nº conjuntos L2 = %ld\n", n_conjuntos_L2);
+    printf("Nº conjuntos L3 = %ld\n\n", total / (tam_linea * vias));    
+}
--- README.md
+++ README.md
@@ -1,3 +1,18 @@
-# ArqComp_P1
-Práctica 1 de Arquitectura de Computadores (Curso 2021-2022, 2º cuatrimestre)
-informe en https://docs.google.com/document/d/1nuKUhfXrs7c5tVJSvDEN7Syz8aPj72he/edit?usp=sharing&ouid=117458461715763844042&rtpof=true&sd=true
+Se incluyen 8 recursos .html correspondientes a gráficas 3D interactivas.
+
+En la raíz aparecen 2 gráficas asociadas al conjunto de datos analizado
+en el informe, para el que se han empleado los valores de D "1, 6, 8, 15, 71". 
+
+Aquella terminada en D corresponde a una ejecución con el array de índices ind[] 
+desordenado y empleando la opción -O0 en la compilación. 
+
+La terminada en O corresponde a una ejecución con el array de índices ind[] ordenado y 
+empleando la opción -O3 en la compilación. 
+
+En la subcarpeta "Graficas 3D extra" aparecen a mayores otras 6 gráficas asociadas a
+datos probados con valores de D que se han estimado interesantes. Cada conjunto de valores
+de D se usa en 2 gráficas, una con ind[] desordenado y la opción -O0 (D), y otra con
+ind[] ordenado y la opción -O3 (O). Los conjuntos de valores de D aquí incluidos son
+"1, 3, 7, 8, 16", "10, 30, 50, 70, 90" y "14, 15, 16, 17, 18".
+
+En el apéndice anexo hay disponibles imágenes de otros tipos de gráficas para estos valores.
